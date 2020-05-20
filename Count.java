a87777de88e528ef7ec7b1dfb613ece693ad4a45
Riel <robert.bor@greenfoxacademy.com>
2020-05-20 11:11:10 +0200

    a4

--- Count.java
+++ Count.java
@@ -1,11 +1,6 @@
 public class Count{
 
-<<<<<<< Updated upstream
-	public void count(int age){
-	System.out.println("My age is " + age);
-=======
 	public void count(){
-	System.out.println(1978);
->>>>>>> Stashed changes
-}
+		System.out.println("My age is " + 1978);
+	}
 }
