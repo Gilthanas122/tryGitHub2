762857170ab7b28a516179c34303c06f70fb4d38
Riel <robert.bor@greenfoxacademy.com>
2020-05-20 11:23:24 +0200

    a5

--- Greet.java
+++ Greet.java
@@ -3,8 +3,14 @@ public class Third {
         greet("riel", "big boss");
     }
 
+<<<<<<< HEAD
     public static void greet(String a, String b){
         sout(a);
         sout(b);
+=======
+    public static void greet(String a){
+        sout"Hi there, beautiful "+ a);
+
+>>>>>>> 0a1dceaa5f3c8978e1e159f21428904033f68097
     }
 }
